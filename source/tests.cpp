8c14008375a5817697ed17ffe8ba82e612091757
Snoopyxxel <snoopyxxel@gmail.com>
2019-05-05 18:43:06 +0200

    + Aufgabe 2.2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,27 @@
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include "vec2.hpp"
 
-int main(int argc, char *argv[])
-{
-  return Catch::Session().run(argc, argv);
+
+SCENARIO("Vector-Initialisierung", "[vector]"){
+    GIVEN("Eine Initialisierung ohne Parameter"){
+        Vec2 v;
+
+        REQUIRE(v.x == 0.0f);
+        REQUIRE(v.y == 0.0f);
+    }
+    GIVEN("Eine Aggregatinitialisierung mit Parametern"){
+        Vec2 v{1.2,3.5};
+
+        REQUIRE(v.x == Approx(1.2));
+        REQUIRE(v.y == Approx(3.5));
+    }
+    /*
+    GIVEN("Eine nicht-Aggregatinitialisierung"){
+        Vec2 v(1.2,3.5);
+
+        REQUIRE(v.x == 0.0f);
+        REQUIRE(v.y == 0.0f);
+    }
+     */
 }
---
+++ source/vec2.hpp
@@ -0,0 +1,12 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+
+// Vec2 data type definition
+struct Vec2 {
+    float x=0.0f;
+    float y=0.0f;
+/* TODO add member variables with
+    default member initialisation */
+};
+
+#endif //VEC2_HPP
